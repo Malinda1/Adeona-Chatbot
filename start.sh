5c0c61af360af7b7d672ed9fd2db7a5bba79f22a
Pasindu malinda <135255199+Malinda1@users.noreply.github.com>
2025-09-29 10:25:43 +0530

    Make some changes to star.sh file

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 # Install dependencies
 
-pip install -r requirements.txt
+pip install -r /Users/pasindumalinda/AI_projects/Agent_02/adeona-chatbot/backend/requirements.txt
 
 # Start FastAPI with uvicorn
 exec export PORT=8000
